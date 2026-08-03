40326592fdc1ab50be8c6fdad6c9321fdfedb1c9
Dristy jain <jaindristy205265@gmail.com>
2026-08-04 05:04:50 +0530

    Add files via upload

---
+++ DSA/Lec22.java
@@ -0,0 +1,96 @@
+public class Lec22 {
+    // public static void main(String[] args) {
+        //String str="hello";
+        // String str1=new String("hello");
+        //String str2="hello";
+        // String str3=new String("hello");
+        // System.out.println(str==str1);
+        // System.out.println(str2==str);
+        // System.out.println(str3==str1);
+
+        // System.out.println(str);
+        // System.out.println(str1);
+        // System.out.println(str2);
+        // System.out.println(str3);
+        // System.out.println(str.length());
+        // System.out.println(str.charAt(2));
+        // String s1="hye";
+        // String s2="bye";
+       // System.out.println(s1+s2);
+        // System.out.println(s1=s2.concat(s1));
+        // String stra=new String("hello");
+        // String strb=new String("bye");
+          
+    //}
+
+    // public static void main(String[] args) {
+    //     String str="hello";
+    //     String str2="hello"; 
+    //     System.out.println(equals(str, str2));
+    // }
+    // public static boolean equals(String s1,String s2){
+    //     if(s1==s2){
+    //         return true;
+    //     }
+    //     if(s1.length()==s2.length()){
+    //         return false;
+    //     }
+    //     for(int i=0;i<s1.length();i++){
+    //         if(s1.charAt(i)!=s2.charAt(i)){
+    //         return false;
+    //     }}
+    //     return true;  
+    // }
+
+    // public static void main(String[] args) {
+
+    //     String s1="hyehellobye";
+    //    System.out.println(s1.substring(0,3));
+    //    System.out.println(s1.substring(2));
+    // String str="riya";
+    // System.out.println(pal(str));
+
+    //}
+// public static boolean pal(String str){
+//     int i=0;
+// int j=str.length()-1;
+//     while(i<j){
+// if(str.charAt(i)!=str.charAt(j)){
+//     return false;
+// }else{
+//      i++;
+//     j--;
+// }}return true;
+// }
+//  public static void main(String[] args) {
+//   String s="hello";
+//   // System.out.println(10+30+s+20);
+//     abc(s);
+
+// }
+//  public static void abc(String s){
+// for(int i=0;i<s.length();i++){
+//   for(int j=0;j<=s.length();j++){
+//     System.out.println(s.substring(i,j));
+//   }
+// }
+// }
+
+public static void main(String[] args) {
+  String s1="kamal";
+  String s2="komal";
+  // System.out.println(10+30+s+20);
+    System.out.println(compareto(s1,s2));
+
+}
+ public static int compareto (String s1,String s2){
+  if(s1==s2){
+    return 0;
+  }
+for(int i=0;i<Math.min(s1.length(),s2.length());i++){
+    if(s1.charAt(i)!=s2.charAt(i)){
+      return s1.charAt(i)-s2.charAt(i);
+    }
+}return s1.length()-s2.length();
+}
+}
